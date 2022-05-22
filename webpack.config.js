336b9e07c35bc5203b33a299b01d71584fb6746a
Aleksey Timofeev <a.timofeev@vmb-team.com>
2022-05-22 14:36:17 +0300

    Update dependences Add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,12 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
   }
 };
